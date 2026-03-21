92b1ae42abefb48be4551f7466efbd42d255aad8
richardscull <106016833+richardscull@users.noreply.github.com>
2026-03-21 19:02:52 +0200

    feat: Add bancho search support

--- server/src/core/domains/osu.ppy.sh/bancho.client.ts
+++ server/src/core/domains/osu.ppy.sh/bancho.client.ts
@@ -1,3 +1,6 @@
+import qs from "qs";
+
+import { BEATMAPS_SEARCH_MAX_RESULTS_LIMIT } from "../../../types/general/api";
 import type { Beatmap, Beatmapset } from "../../../types/general/beatmap";
 import logger from "../../../utils/logger";
 import { BaseClient } from "../../abstracts/client/base-client.abstract";
@@ -8,12 +11,16 @@ import type {
   GetBeatmapsetsByBeatmapIdsOptions,
   GetBeatmapsOptions,
   ResultWithStatus,
+  SearchBeatmapsetsOptions,
 } from "../../abstracts/client/base-client.types";
 import {
   ClientAbilities,
 } from "../../abstracts/client/base-client.types";
 import { BanchoService } from "./bancho-client.service";
-import type { BanchoBeatmap, BanchoBeatmapset } from "./bancho-client.types";
+import type { BanchoBeatmap, BanchoBeatmapset, BanchoBeatmapsetSearchResult } from "./bancho-client.types";
+
+const BANCHO_SEARCH_PAGE_SIZE = 50;
+const BANCHO_SEARCH_PAGES_LIMIT = 200;
 
 export class BanchoClient extends BaseClient {
   private readonly banchoService = new BanchoService(this.baseApi);
@@ -29,6 +36,7 @@ export class BanchoClient extends BaseClient {
           ClientAbilities.GetBeatmaps,
           ClientAbilities.DownloadOsuBeatmap,
           ClientAbilities.GetBeatmapsetsByBeatmapIds,
+          ClientAbilities.SearchBeatmapsets,
         ],
       },
       {
@@ -45,6 +53,7 @@ export class BanchoClient extends BaseClient {
               ClientAbilities.GetBeatmaps,
               ClientAbilities.DownloadOsuBeatmap,
               ClientAbilities.GetBeatmapsetsByBeatmapIds,
+              ClientAbilities.SearchBeatmapsets,
             ],
             routes: ["/"],
             limit: 1200,
@@ -158,6 +167,61 @@ export class BanchoClient extends BaseClient {
     }
 
     return { result: result.data, status: result.status };
+  }
+
+  async searchBeatmapsets(
+    ctx: SearchBeatmapsetsOptions,
+  ): Promise<ResultWithStatus<Beatmapset[]>> {
+    const page = Math.floor((ctx.offset ?? 0) / BANCHO_SEARCH_PAGE_SIZE) + 1;
+
+    if (page > BANCHO_SEARCH_PAGES_LIMIT
+      || (ctx.limit && ctx.limit > BANCHO_SEARCH_PAGE_SIZE && page === BANCHO_SEARCH_PAGES_LIMIT)) {
+      return { result: null, status: 500 }; // Bancho API has a limit of 200 pages
+    }
+
+    const result = await this.api.get<BanchoBeatmapsetSearchResult>(`api/v2/beatmapsets/search`, {
+      config: {
+        headers: {
+          Authorization: `Bearer ${await this.osuApiKey}`,
+        },
+        params: {
+          query: ctx.query,
+          page,
+          status: ctx.status,
+          mode: ctx.mode,
+        },
+        paramsSerializer: params =>
+          qs.stringify(params, { indices: false }),
+      },
+    });
+
+    if (!result || result.status !== 200 || !result.data) {
+      return { result: null, status: result?.status ?? 500 };
+    }
+
+    let { beatmapsets } = result.data;
+    let additionalBeatmapsets: BanchoBeatmapset[] = [];
+
+    if (ctx.limit && ctx.limit <= BEATMAPS_SEARCH_MAX_RESULTS_LIMIT) {
+      if (ctx.limit < BANCHO_SEARCH_PAGE_SIZE) {
+        beatmapsets = beatmapsets.slice(0, ctx.limit);
+      }
+
+      if (ctx.limit > BANCHO_SEARCH_PAGE_SIZE && page < BANCHO_SEARCH_PAGES_LIMIT) {
+        additionalBeatmapsets = await this.searchBeatmapsets({
+          ...ctx,
+          limit: ctx.limit - beatmapsets.length,
+          offset: (ctx.offset ?? 0) + beatmapsets.length,
+        }).then(result => result.result ?? []);
+      }
+    }
+
+    return {
+      result: [...additionalBeatmapsets, ...(beatmapsets.map((b: BanchoBeatmapset) =>
+        this.convertService.convertBeatmapset(b),
+      ))],
+      status: result.status,
+    };
   }
 
   private async getBeatmapSetById(
